7e9faea8544c8df4b455a5954a2ece5e47ed2a0d
mahongquan <mahongquan@sina.com>
2017-06-02 08:52:52 +0800

    ui

---
+++ src/UserComponent.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+
+export default class UserComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false,
+      logined:false,
+      user:"mahongquan",
+    };
+  }
+
+  handleTouchTap = (event) => {
+    // This prevents ghost click.
+    event.preventDefault();
+
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget
+    });
+  };
+  handleLogin = () => {
+  	console.log("login");
+    this.setState({
+      logined: true,
+    });
+  };
+  handleLogout = () => {
+  	console.log("logout");
+    this.setState({
+      logined: false,
+    });
+  };
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <RaisedButton  onTouchTap={this.handleTouchTap}
+          label={this.state.user}>
+          <span className="caret"> </span>
+        </RaisedButton>
+        <Popover
+          open={this.state.open}
+          anchorEl={this.state.anchorEl}
+          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+          targetOrigin={{horizontal: 'left', vertical: 'top'}}
+          onRequestClose={this.handleRequestClose}
+        >
+          <Menu>
+            <MenuItem primaryText="登录" disabled={this.state.logined} onTouchTap={this.handleLogin} />
+            <MenuItem primaryText="注销" disabled={!this.state.logined} onTouchTap={this.handleLogout} />
+          </Menu>
+        </Popover>
+      </div>
+    );
+  }
+}
