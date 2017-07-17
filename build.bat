f235ae239f8e923fe3565fd8dd9f62905b185b4d
mahongquan <mahongquan@sina.com>
2017-07-17 14:55:42 +0800

    remove css link

--- build.bat
+++ build.bat
@@ -1,4 +1,1 @@
-rem node_modules\.bin\react-scripts build
-copy /Y build\index.html d:\parts\static\index.html
-copy /Y build\static\js\*.js d:\parts\static\js\
-copy /Y build\static\css\*.css d:\parts\static\css\
+node_modules\.bin\react-scripts build
---
+++ c.bat
@@ -0,0 +1,3 @@
+copy /Y build\index.html d:\parts\static\index.html
+copy /Y build\static\js\*.js d:\parts\static\js\
+copy /Y build\static\css\*.css d:\parts\static\css\
